eff66ccec1edf76bf56380d94ee035b5cf1536cb
remyallen <remy.a@icloud.com>
2016-04-21 11:19:09 -0500

    fix conflict

--- modules/connection.js
+++ modules/connection.js
@@ -3,7 +3,8 @@ var connectionString = '';
 if(process.env.DATABASE_URL !== undefined) {
     connectionString = process.env.DATABASE_URL + '?ssl=true';
 } else {
-    connectionString = 'postgres://dqqhueitqjswre:wgvCElpoXCoYBkANPhDXVYxjLp@ec2-23-21-215-184.compute-1.amazonaws.com:5432/dfdvljek5oqofi';
+    connectionString = 'postgres://localhost:5432/bet_shalom';
+    //connectionString = 'postgres://dqqhueitqjswre:wgvCElpoXCoYBkANPhDXVYxjLp@ec2-23-21-215-184.compute-1.amazonaws.com:5432/dfdvljek5oqofi';
 }
 
 module.exports = connectionString;
